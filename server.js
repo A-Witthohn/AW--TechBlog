1fc02ce6fba42529083678ce5fd7ca09f2a8e1d4
Aaron <awitthohn@yahoo.com>
2023-07-20 21:26:41 -0400

    changed express handlebars version, built template for all handlebar pages debugged issue with pages not rendering

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 // server.js
 const express = require('express');
-const session = require('./config/session'); // Import the session middleware
+// Import the session middleware
+const session = require('./config/session');
+const exphbs = require('express-handlebars');
+
+
+
+
 const app = express();
+
+
 
 // Use the session middleware
 app.use(session);
+
 
 // ... Other middleware and routes ...
 
@@ -13,3 +22,33 @@ app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}. Check it out here http://localhost:${PORT} !`);
 });
 
+
+//setup handlebars 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Renders home or root
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Render dashboard 
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard'); 
+});
+
+// Render login 
+app.get('/login', (req, res) => {
+  res.render('login'); 
+});
+
+// Render signup 
+app.get('/signup', (req, res) => {
+  res.render('signup'); 
+});
+
+// Render individual post 
+app.get('/post/:postId', (req, res) => {
+  const postId = req.params.postId;
+  res.render('post', postData); 
+});
